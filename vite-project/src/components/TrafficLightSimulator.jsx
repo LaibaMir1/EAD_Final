ac244d207c1093d9154f8be50aa605255ada2b17
LaibaMir1 <laaaiba.mir119@gmail.com>
2025-06-03 11:24:29 +0500

    Refactor: convert to fucntional react component

--- vite-project/src/components/TrafficLightSimulator.jsx
+++ vite-project/src/components/TrafficLightSimulator.jsx
@@ -3,13 +3,29 @@ import React,{useState} from 'react'
 const TrafficLightSimulator = () => {
     //useState for saving state of ligt 
     const [currentLight, setLight] = useState('red'); 
-  
+     
+    useEffect(()=>{
+        const cycle = ['red', 'yellow', 'green']; 
+        const interval = setInterval(()=>{
+            setLight(prev =>{
+                const next = (cycle.indexOf(prev)+1)
+                return cycle[next]; 
+            })
+        }, 3000)
+
+
+      return () => clearInterval(interval);
+    }, []);
+
   return (<>
-    {/* //light containers  */}
+
+
     <div className="container">
       <div className="light-red" />
       <div className="light-yellow}" />
       <div className="light-green" />
+
+
     </div>
     </>
   )
