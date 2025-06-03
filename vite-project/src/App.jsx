86493894f07e3d4dd070c93cbe127905c87f5aad
LaibaMir1 <laaaiba.mir119@gmail.com>
2025-06-03 12:05:03 +0500

    Style: enhance UI with transitions and active styles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <TrafficLightSimulator></TrafficLightSimulator>
+    <TrafficLightSimulator/>
 
 
 
--- vite-project/src/components/TrafficLightSimulator.jsx
+++ vite-project/src/components/TrafficLightSimulator.jsx
@@ -1,24 +1,70 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
 const TrafficLightSimulator = () => {
-    //useState for saving state of ligt 
-    const [currentLight, setLight] = useState('red'); 
-     
+    
+    const [currentLight , setLight] = useState('red'); 
+
     useEffect(()=>{
-        const cycle = ['red', 'yellow', 'green']; 
-        const interval = setInterval(()=>{
-            setLight(prev =>{
-                const next = (cycle.indexOf(prev)+1)
-                return cycle[next]; 
-            })
-        }, 3000)
-      return () => clearInterval(interval);
-    }, []);
+        const cycle = ['red', 'yellow','green']; 
+        
+        setLight(prev =>{
+            const next = (cycle.prev+1)
+            return cycle[next]; 
+        }
+
+        ), 3000}
+    return () => clearInterval(interval); 
+    }, []); 
+
 
   return (<>
-    
+   <style>{`
+        .traffic-light {
+          width: 50px;
+          background: #333;
+          padding: 20px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 40px; 
+        }
+
+        .light {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+          background: black;
+          transition: background 0.5s, box-shadow 0.5s;
+        }
+
+        .light.red.active {
+          background: red;
+          box-shadow: 0 0 15px red;
+        }
+
+        .light.yellow.active {
+          background: yellow;
+          box-shadow: 0 0 15px yellow;
+        }
+
+        .light.green.active {
+          background: limegreen;
+          box-shadow: 0 0 15px limegreen;
+        }
+      `}
+      </style>
+
+  {/* Traffic light */}
+    <div className="traffic-light">
+      <div className={`light red ${currentLight === 'red' ? 'active' : ''}`} />
+      <div className={`light yellow ${currentLight === 'yellow' ? 'active' : ''}`} />
+      <div className={`light green ${currentLight === 'green' ? 'active' : ''}`} />
+    </div>
+
     </>
-  )
+  ); 
 }
 
-export default TrafficLightSimulator
+export default TrafficLightSimulator;
+
+
